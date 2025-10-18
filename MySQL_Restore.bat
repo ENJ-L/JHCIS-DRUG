b69876ddc7c35ef9b7ea6039715e104c45c294b3
ENJ-L <narawith2540@gmail.com>
2025-10-18 11:10:19 +0700

    Update MySQL restore script with new file name

--- MySQL_Restore.bat
+++ MySQL_Restore.bat
@@ -1,3 +1,6 @@
-Restore
+:: 1. Locate the Mysql database 
+cd "C:\Program Files\jhcis\MySQL5.6\bin"
 :: ======================================================================
-mysql -u root -p123456 -P3333 jhcisdb < "C:\Scripts\BackupData\ชื่อไฟล์.sql"
+:: 2. Restore
+mysql -u root -p123456 -P3333 jhcisdb < "C:\Scripts\BackupData\name-of-backup.sql"
+pause
